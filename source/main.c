e4775c28338ec141c66894bc2e7a7be8b1d386be
Andrew Bazua <abazu001@wch136-19.cs.ucr.edu>
2019-10-28 13:36:48 -0700

    Initial Commit

---
+++ source/main.c
@@ -0,0 +1,156 @@
+/*	Author: abazu001
+ *  Partner(s) Name: 
+ *	Lab Section: 23
+ *	Assignment: Lab #7  Exercise #1
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include "io.h"
+
+typedef enum States {init, wait, increment, decrement, reset} States;
+
+volatile unsigned char TimerFlag = 0; // TimerISR() sets this to 1. C programmer should clear to 0.
+
+unsigned long _avr_timer_M = 1; // Start count from here, down to 0. Default 1 ms.
+unsigned long _avr_timer_cntcurr = 0;   // Current internal count of 1 ms ticks.
+
+void TimerOn() {
+    TCCR1B = 0x0B;
+    
+    OCR1A = 125;
+    
+    TIMSK1 = 0x02;
+    
+    TCNT1=0;
+    
+    _avr_timer_cntcurr = _avr_timer_M;
+
+    SREG |= 0x80;
+    
+}
+
+void TimerOff() {
+    TCCR1B = 0x00;
+}
+
+void TimerISR() {
+    TimerFlag = 1;
+}
+
+ISR(TIMER1_COMPA_vect) {
+    // CPU automatically calls when TCNT1 == OCR1 (every 1 ms per TimerOn settings)
+    _avr_timer_cntcurr--;   // Count down to 0 rather than up to TOP
+    if (_avr_timer_cntcurr == 0) {
+        TimerISR();
+        _avr_timer_cntcurr = _avr_timer_M;
+    }
+
+}
+
+void TimerSet(unsigned long M) {
+    _avr_timer_M = M;
+    _avr_timer_cntcurr = _avr_timer_M;
+}
+
+int Tick(int state) {
+    static unsigned char count; 
+    static unsigned char timeCntr;   
+
+    unsigned char A0 = ~PINA & 0x01;
+    unsigned char A1 = ~PINA & 0x02;
+
+    switch (state) {
+        case init:
+            state = wait;
+            break;
+        
+        case wait:
+            if (A0 && !A1) { state = increment; }
+            else if (!A0 && A1) { state = decrement; }
+            else if (!A0 && !A1) { state = decrement; }
+            else { state = wait; }
+            break;
+
+        case increment:
+            if (A0 && !A1) { state = increment; }
+            else if (A0 && A1) { state = reset; }
+            else if (!A0 && !A1) { state = wait; }
+            break;
+
+        case decrement: 
+            if (!A0 && A1) { state = decrement; }
+            else if (A0 && A1) { state = reset; }
+            else if (!A0 && !A1) { state = wait; }
+            break;
+
+        case reset:
+            state = (A0 || A1) ? reset: wait;
+            break;
+        
+    }
+
+    switch (state) {
+        case init:
+            count = 0x00;
+            timeCntr = 0x00;
+            break;
+        
+        case wait: 
+            timeCntr = 0;
+            break;
+
+        case increment:
+            if (count < 9) {                
+                if (timeCntr == 4) { ++count; }
+                ++count;
+            }
+            ++timeCntr;
+            break;
+
+        case decrement:
+            if (count > 0) {                
+                if (timeCntr == 4) { --count; }
+                --count;
+            }
+            ++timeCntr;
+            break;
+
+        case reset: 
+            count = 0;
+            break;
+    }
+    
+    LCD_WriteData(1, count + '0');
+
+    PORTC = count;
+    
+    return state;
+}
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+    DDRA = 0x00; PORTA = 0xFF;
+	DDRC = 0xFF; PORTC = 0x00;
+    DDRD = 0xFF; PORTD = 0x00;
+    /* Insert your solution below */
+    
+    TimerSet(250);
+    TimerOn();
+    LCD_init();  
+
+    States state = init;
+    
+    while (1) {
+        state = Tick(state);
+        
+        while (!TimerFlag)
+        TimerFlag = 0;
+    }
+    return 1;
+}
+
+
